a8d65200d3bccdd25ee5743d6e2a0af58c5653c5
Arie Brainware <publicu001@protonmail.com>
2022-07-06 23:53:56 +0700

    Adjust webhook URL

--- config/telegram.php
+++ config/telegram.php
@@ -36,7 +36,7 @@ return [
             'username'            => '@WifiScannerRitalin_bot',
             'token'               => env('TELEGRAM_BOT_TOKEN', '5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0'),
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
-            'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'https://api.telegram.org/bot5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/setWebhook?url=https://ritalin-wifiscanner.herokuapp.com/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook'),
+            'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'https://ritalin-wifiscanner.herokuapp.com/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook'),
             'commands'            => [
                 // Telegram\Bot\Commands\HelpCommand::class,
                 App\Http\Controllers\StartCommand::class,
