04d820b9ecbf422412ba59ad1103ad670a2be414
ricky <59864164+ricky603@users.noreply.github.com>
2022-07-18 14:22:21 +0700

    aaa

--- routes/web.php
+++ routes/web.php
@@ -47,24 +47,28 @@ Route::post('/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook', function 
     //     ]);
     // }
     $response = json_decode($request->getContent());
-    if ($response['text'] == '//start') {
-        $data = [
-            'chat_id' => $response['message']['chat']['id'],
-            'text' => 'kita start ya..'
-        ];
-    } else if ($response['text'] == '/stop') {
-        $data = [
-            'chat_id' => $response['message']['chat']['id'],
-            'text' => 'kita stop ya..'
-        ];
-    } else if ($response['text'] == '/help') {
-        $data = [
-            'chat_id' => $response['message']['chat']['id'],
-            'text' => 'kita bantu ya..'
-        ];
-    }
+    // if ($response['text'] == '//start') {
+    //     $data = [
+    //         'chat_id' => $response['message']['chat']['id'],
+    //         'text' => 'kita start ya..'
+    //     ];
+    // } else if ($response['text'] == '/stop') {
+    //     $data = [
+    //         'chat_id' => $response['message']['chat']['id'],
+    //         'text' => 'kita stop ya..'
+    //     ];
+    // } else if ($response['text'] == '/help') {
+    //     $data = [
+    //         'chat_id' => $response['message']['chat']['id'],
+    //         'text' => 'kita bantu ya..'
+    //     ];
+    // }
 
-        Telegram::sendMessage($data);
+    //     Telegram::sendMessage($data);
+    Telegram::sendMessage([
+        'chat_id' => '-1001741565449',
+        'text' => $response['message']['chat']['id'],
+    ])
     return 'ok';
 });
 
