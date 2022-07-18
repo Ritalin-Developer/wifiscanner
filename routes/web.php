542f85a877c937da0a9880a7f6607824a4c0981c
ricky <59864164+ricky603@users.noreply.github.com>
2022-07-18 11:56:21 +0700

    aa

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,8 @@ Route::prefix('wifiscanner')->group(function () {
 
 // Laravel
 Route::post('/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook', function () {
-    $update = Telegram::commandsHandler(true);
+    $update = Telegram::commandsHandler(false);
+    return dd($update);
 });
 
 // Standalone
