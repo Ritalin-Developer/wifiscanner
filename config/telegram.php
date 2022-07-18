677e8400cdbe8a6c108427d86e686fd5a3a3f94b
ricky <59864164+ricky603@users.noreply.github.com>
2022-07-18 11:14:42 +0700

    aaa

--- config/telegram.php
+++ config/telegram.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Telegram\Commands\HelpCommand;
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -38,7 +40,7 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'https://ritalin-wifiscanner.herokuapp.com/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook'),
             'commands'            => [
-                App\Telegram\Commands\HelpCommand::class,
+                HelpCommand,
             ]
         ]
     ],
