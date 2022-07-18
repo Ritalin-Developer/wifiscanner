1dc4a17483560f225680bb123333ff1843681731
ricky <59864164+ricky603@users.noreply.github.com>
2022-07-18 13:26:25 +0700

    aaa

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,11 @@ Route::prefix('wifiscanner')->group(function () {
 
 // Laravel
 Route::post('/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook', function (Request $request) {
-
-    return dd($request->getContent()[-1]);
+    $ResponseData = $request->getContent();
+    // $ResponseData = json
+    foreach ($Response as $res) {
+        return dd($res['message']['entities'][0]['type']);
+    }
 });
 
 // Standalone
