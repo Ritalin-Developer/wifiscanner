24d79bf37c3012a560a1515382f2e969d110c39b
ricky <59864164+ricky603@users.noreply.github.com>
2022-07-25 17:34:35 +0700

    aaaaa

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/bot/getupdates', function() {
     $data = '{"update_id":230189237,"message":{"message_id":127,"from":{"id":1024615671,"is_bot":false,"first_name":"Ricky","username":"Rickyfishboy","language_code":"en"},"chat":{"id":1024615671,"first_name":"Ricky","username":"Rickyfishboy","type":"private"},"date":1658742731,"text":"/stop","entities":[{"offset":0,"length":5,"type":"bot_command"}]}}';
     $data = json_decode($data, true);
     // return dd($data);
-    return dd($data['message']['chat']['id'], $data['message']['text']);
+    return dd($data['message']['from']['id'], $data['message']['text']);
 });
 
 Route::prefix('wifiscanner')->group(function () {
@@ -34,35 +34,39 @@ Route::prefix('wifiscanner')->group(function () {
 // Laravel
 Route::post('/5465295406:AAH_GzsIj6xd2IPukMhK-c1GJzpQQpCWHm0/webhook', function (Request $request) {
     $response = json_decode($request->getContent(), true);
-    if ($response['message']['text'] == '/start') {
-        $response = Http::get('http://10.30.23.61/status');
-        $wifiList = json_decode(str_replace("'", '"', $response->body()), true);
+    $data = [
+        'chat_id' => -1001741565449,
+        'text' => 'aaaaaa'
+    ];
+    // if ($response['message']['text'] == '/start') {
+    //     $response = Http::get('http://10.30.23.61/status');
+    //     $wifiList = json_decode(str_replace("'", '"', $response->body()), true);
 
-        $botChatText = "WiFi Status ";
+    //     $botChatText = "WiFi Status ";
 
-        foreach ($wifiList as $key => $wifi) {
-            $botChatText = $botChatText.($key + 1)."\n";
-            $botChatText = $botChatText."SSID : ".$wifi["SSID"]."\n";
-            $botChatText = $botChatText."RSSI : ".$wifi["RSSI"]."\n";
-            $botChatText = $botChatText."MAC : ".$wifi["MAC"]."\n";
-            $botChatText = $botChatText.$wifi["isSecured"]."\n\n";
-        }
+    //     foreach ($wifiList as $key => $wifi) {
+    //         $botChatText = $botChatText.($key + 1)."\n";
+    //         $botChatText = $botChatText."SSID : ".$wifi["SSID"]."\n";
+    //         $botChatText = $botChatText."RSSI : ".$wifi["RSSI"]."\n";
+    //         $botChatText = $botChatText."MAC : ".$wifi["MAC"]."\n";
+    //         $botChatText = $botChatText.$wifi["isSecured"]."\n\n";
+    //     }
 
-        $data = [
-            'chat_id' => $response['message']['chat']['id'],
-            'text' => $botChatText
-        ];
-    } else if ($response['message']['text'] == '/stop') {
-        $data = [
-            'chat_id' => $response['message']['from']['id'],
-            'text' => 'kita stop ya..'
-        ];
-    } else if ($response['message']['text'] == '/help') {
-        $data = [
-            'chat_id' => $response['message']['chat']['id'],
-            'text' => 'kita bantu ya..'
-        ];
-    }
+    //     $data = [
+    //         'chat_id' => $response['message']['chat']['id'],
+    //         'text' => $botChatText
+    //     ];
+    // } else if ($response['message']['text'] == '/stop') {
+    //     $data = [
+    //         'chat_id' => $response['message']['from']['id'],
+    //         'text' => 'kita stop ya..'
+    //     ];
+    // } else if ($response['message']['text'] == '/help') {
+    //     $data = [
+    //         'chat_id' => $response['message']['chat']['id'],
+    //         'text' => 'kita bantu ya..'
+    //     ];
+    // }
 
     Telegram::sendMessage($data);
     return 'ok';
